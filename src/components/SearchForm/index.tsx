143ab3485c0cbdff723c619cec6843f093d48b8b
dongxiaoming <dongxiaoming@jiuxiniot.com>
2020-07-23 20:55:04 +0800

    完成分层审核清单

---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode, useCallback } from 'react';
+import { Form, Input, Select, Button } from 'antd';
+import { Callbacks, Store } from 'rc-field-form/lib/interface';
+import { FormItemProps } from 'antd/lib/form'
+
+const { Option } = Select;
+
+interface TypeOption {
+  value: string | number;
+  name: string;
+}
+
+interface WithTypeItemProps extends FormItemProps {
+  type: 'input' | 'select';
+  typeOptions?: TypeOption | any;
+} 
+
+interface Props {
+  items: WithTypeItemProps[];
+  initialValues?: Store;
+  name?: string;
+  onFinish?:Callbacks['onFinish'];
+  onFinishFailed?:Callbacks['onFinishFailed'];
+}
+
+const SearchForm: React.FC<Props> = ({
+  items,
+  name,
+  initialValues,
+  onFinish,
+  onFinishFailed,
+}) => {
+  const [form] = Form.useForm();
+  const handleReset = useCallback(
+    () => {
+      form.resetFields();
+    },
+    [],
+  )
+  return (
+    <Form
+      form={form}
+      layout="inline"
+      name={name}
+      initialValues={initialValues || {}}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      {items.map(({ type, typeOptions, ...itemProps }) => (
+        <Form.Item {...itemProps}>
+          {getComByType(type, typeOptions)}
+        </Form.Item>
+      ))}
+      <Form.Item>
+        <Button htmlType="submit" type="primary">查询</Button>
+      </Form.Item>
+      <Form.Item>
+        <Button onClick={handleReset}>重置</Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+function getComByType(type: string, typeOptions?: any): ReactNode {
+  switch(type) {
+    case 'input':
+      return <Input />
+    case 'select':
+      return <Select style={{width: 180}}>
+        {typeOptions?.map(({ value, name }: TypeOption) => (
+          <Option key={value} value={value}>{name}</Option>
+        ))}
+      </Select>
+    default:
+      return null;
+  }
+}
+
+export default SearchForm;
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useCallback } from 'react';
 import { connect } from 'dva';
 import { Dispatch } from 'redux';
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 
 import { StateType } from './model';
 import Main from '@/components/MainContainer';
-import Search from '@/components/Search';
-import ExportButton from '@/components/ExportButton';
+import SearchForm from '@/components/SearchForm';
 
 interface Props {
   dispatch: Dispatch;
@@ -16,32 +15,82 @@ const List: React.FC<Props & StateType> = ({
   dispatch,
 }) => {
 
-  const onChange = useCallback((e) => {
-  }, [])
-
-  const reset = useCallback(() => {
-  }, [])
-
-  const onSearch = useCallback(() => {
+  const onSearch = useCallback((values) => {
+    console.log('onSearch:onFinish', values)
   }, [])
 
   return (
     <Main
       search={
-        <Search
-          value={''}
-          onChange={onChange}
-          reset={reset}
-          onSearch={onSearch}
+        <SearchForm
+          items={[
+            {
+              label: "审核时间",
+              name: 'auditTime',
+              type: 'input',
+            },
+            {
+              label: "审核范围",
+              name: 'auditScope',
+              type: 'select',
+              typeOptions: [
+                {
+                  name: 'value1',
+                  value: 1,
+                },
+                {
+                  name: 'value2',
+                  value: 2,
+                },
+                {
+                  name: 'value3',
+                  value: 3,
+                },
+                {
+                  name: 'value4',
+                  value: 4,
+                }
+              ],
+            }
+          ]}
+          initialValues={{
+            auditScope: 1
+          }}
+          onFinish={onSearch}
         />
       }
-      extra={<ExportButton />}
+      control={<Button type="primary">今日审核</Button>}
     >
       <Table
         bordered
         size="small"
         pagination={false}
-        columns={[]}
+        columns={[
+          {
+            title: '审核时间',
+            dataIndex: 'auditTime',
+            key: 'auditTime',
+            align: 'center',
+          },
+          {
+            title: '审核范围',
+            dataIndex: 'auditScope',
+            key: 'auditScope',
+            align: 'center',
+          },
+          {
+            title: '审核人',
+            dataIndex: 'createUser',
+            key: 'createUser',
+            align: 'center',
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            align: 'center',
+          }
+        ]}
         dataSource={[]}
         rowKey={({ processId }) => processId}
       />
